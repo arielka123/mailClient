88c7eab73eb7186ffe4c38e5a0492ec821adfa3d
Arleta Madej <arleta.madej@gmail.com>
2023-07-28 16:12:44 +0200

    adding messages to folders

--- src/com/project/model/EmailTreeItem.java
+++ src/com/project/model/EmailTreeItem.java
@@ -1,14 +1,54 @@
 package com.project.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
+
+import javax.mail.Flags;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public class EmailTreeItem <String> extends TreeItem {
 
     private String name;
+    private ObservableList<EmailMessage> emailMessages;
+    private int unreadMassagesCount;
 
     public EmailTreeItem(String name) {
         super(name);
         this.name = name;
+        this.emailMessages = FXCollections.observableArrayList();
+    }
+
+    public void addEmail(Message message) throws MessagingException {
+        boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
+        EmailMessage emailMessage = new EmailMessage(
+                message.getSubject(),
+                message.getFrom()[0].toString(),
+                message.getRecipients(MimeMessage.RecipientType.TO)[0].toString(),
+                message.getSize(),
+                message.getSentDate(),
+                messageIsRead,
+                message
+                );
+        emailMessages.add(emailMessage);
+        if(!messageIsRead){
+            increamentMessages();
+        }
+        System.out.println("added to "+ name + " " +message.getSubject());
+    }
+
+    private void increamentMessages(){
+        unreadMassagesCount ++;
+        updateName();
+    }
+
+    private void updateName (){
+        if(unreadMassagesCount > 0){
+            this.setValue((String)(name + " (" + unreadMassagesCount + ")"));
+        }
+        else setValue(name);
     }
 
 }
