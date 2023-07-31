c937fff9bf240a2ea48d2260e81d419f3a783f0c
Arleta Madej <arleta.madej@gmail.com>
2023-07-31 17:36:40 +0200

    receiving emails

--- src/com/project/controller/services/FetchFolderService.java
+++ src/com/project/controller/services/FetchFolderService.java
@@ -5,6 +5,7 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 import javax.mail.Folder;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Store;
 import javax.mail.event.MessageCountEvent;
@@ -60,6 +61,14 @@ public class FetchFolderService extends Service<Void> {
         folder.addMessageCountListener(new MessageCountListener() {
             @Override
             public void messagesAdded(MessageCountEvent e) {
+                for(int i=0; i<e.getMessages().length; i++){
+                    try{
+                        Message message = folder.getMessage(folder.getMessageCount()-i);
+                        emailTreeItem.addEmailToTop(message);
+                    }catch (Exception exception){
+                        exception.printStackTrace();
+                    }
+                }
                 System.out.println("message added event " + e);
             }
             @Override
